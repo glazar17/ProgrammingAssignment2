550c465bb32c074953c21364cee1b8ccfbc67643
Geoffrey Lazar <glazar817@gmail.com>
2021-12-09 09:52:12 -0500

    getting ready to push an example cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The cachematrix.R function has a matrix argument and builds
+## a set of functions and returns them to a list in the parent environment
+## A sample matrix: m1<-matrix(c(1/2,-1/4,-1,3/4),nrow=2,ncol=2)
+## Execute: aMatrix<-cachematrix(m1)
+## Execute: aMatrix$get() - will return m1
+## Execute: aMatrix$getinv() - will return NULL
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+cachematrix<- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+            x <<- y
+            m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
-
-
-## Write a short comment describing this function
-
+## The cacheSolve will either solve for the inv of the matrix if m is NULLL
+## or retrieve the inv from the object passed in as the argument
+## Execute: cacheSolve(aMatrix) - will return the inv of m1
+## Execute: cacheSolve(aMatrix) - will return the inv of m1 from
+## the data already calculated and print out that it is getting the cached data
+## Execute: aMatrix$getinv() - will return the inv of m1 already
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    m <- x$getinv()
+    if(!is.null(m)) {
+            print("getting cached data")
+            return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
+    }
